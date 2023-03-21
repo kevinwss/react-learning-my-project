3087254fb8cad0ccee154388a8a63c2385543c99
Kevin <shaoshenwang@gmail.com>
2023-03-22 00:13:20 +1100

    added material-ui bottom navigator and its router

---
+++ src/pages/Favorites/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Favorite extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>This is Favorite!!!!!!!</h1>
+            </div>
+        );
+    }
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,39 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 import News from "../News";
+import Favorites from "../Favorites";
+
+const useStyles = makeStyles({
+    root: {
+        width: 500,
+    },
+});
+
+function SimpleBottomNavigation() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    return (
+        <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+            showLabels
+            className={classes.root}
+        >
+            <BottomNavigationAction component={Link} to="/home/news" label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction component={Link} to="/home/favorites" label="Favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+    );
+}
 
 export default class Home extends React.Component {
     render() {
@@ -10,7 +43,10 @@ export default class Home extends React.Component {
 
                 <Routes>
                     <Route path="/news" element={<News />} />
+                    <Route path="/favorites" element={<Favorites />} />
                 </Routes>
+
+                <SimpleBottomNavigation />
             </div>
         );
     }
